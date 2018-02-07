5da5c6d2fdef61d598941550b6958cc4edea91ad
Angel M De Miguel <angel@bitnami.com>
2018-02-07 12:52:18 +0100

    Add dinghy-proxy command

--- dotfiles/.chest.zsh
+++ dotfiles/.chest.zsh
@@ -94,6 +94,16 @@ dkcsl() {
   dkcs $1 && dkcl $1
 }
 
+# Dinghy HTTP proxy
+alias dinghy-proxy="\
+  docker run -d --restart=always \
+    -v /var/run/docker.sock:/tmp/docker.sock:ro \
+    -v ~/.dinghy/certs:/etc/nginx/certs \
+    -p 80:80 -p 443:443 -p 19322:19322/udp \
+    -e DNS_IP=127.0.0.1 -e CONTAINER_NAME=http-proxy \
+    --name http-proxy \
+    codekitchen/dinghy-http-proxy"
+
 ##### Kubernetes
 
 # Kubectl
