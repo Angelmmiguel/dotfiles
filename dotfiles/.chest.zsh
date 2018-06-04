7245ff89617ea738b55b6f624db0ece4e77226ab
Angel M De Miguel <angel@bitnami.com>
2018-06-04 18:02:23 +0200

    Add the tweet-to-gif method

--- dotfiles/.chest.zsh
+++ dotfiles/.chest.zsh
@@ -174,3 +174,64 @@ st() {
  # Query to stackoverflow
  open "https://stackoverflow.com/search?q=$query"
 }
+
+##### Random
+
+# Get the gif from a tweet!
+# From: https://gist.github.com/brianloveswords/7534169715b5750a892cddcf54c2aa0e
+_video-url-from-tweet() {
+  if [ "$1" ]; then
+    url=$1
+  else
+    echo "Must provide a url"
+    return 1
+  fi
+
+  curl --silent $url |\
+    # should find the <meta> tag with content="<thumbnail url>"
+    (grep -m1 "tweet_video_thumb" ||\
+      echo "Could not find video" && return 1) |\
+
+    # from: <meta property="og:image" content="https://pbs.twimg.com/tweet_video_thumb/xxxxxxxxxx.jpg">
+    # to: https://pbs.twimg.com/tweet_video_thumb/xxxxxxxxxx.jpg
+    cut -d '"' -f 4 |\
+
+    # from: https://pbs.twimg.com/tweet_video_thumb/xxxxxxxxxx.jpg
+    # to: https://video.twimg.com/tweet_video/xxxxxxxxxx.mp4
+    sed 's/.jpg/.mp4/g' |\
+    sed 's/pbs.twimg.com\/tweet_video_thumb/video.twimg.com\/tweet_video/g'
+}
+_video-from-tweet() {
+  if [ "$1" ]; then
+    url=$1
+  else
+    echo "Must provide a url"
+    return 1
+  fi
+  curl $(_video-url-from-tweet $url)
+}
+_video-to-gif() {
+  # derived from https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/
+  if [ "$2" ]; then
+    input=$1
+    output=$2
+  else
+    echo "Must provide an input file and output file"
+    return 1
+  fi
+
+  ffmpeg -i $input \
+          -filter_complex "[0:v] split [a][b];[a] palettegen [p];[b][p] paletteuse" \
+          -f gif \
+          $output
+}
+gif-from-tweet() {
+  if [ "$2" ]; then
+    url=$1
+    output=$2
+  else
+    echo "Must provide a url and an output filename"
+    return 1
+  fi
+  _video-from-tweet $url | _video-to-gif - $output
+}
