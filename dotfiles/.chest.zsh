3196078ee60b2ddf767b9e1e5d7a53e4f8d7772f
Angel M <Angelmmiguel@users.noreply.github.com>
2021-02-07 03:46:15 -0800

    Add missing gp alias

--- dotfiles/.chest.zsh
+++ dotfiles/.chest.zsh
@@ -157,6 +157,7 @@ cert_expiration() {
 # Some aliases
 alias gs="git status"
 alias ga="git add"
+alias gp="git push"
 alias glc="git log -1 --pretty=%B"
 alias gb="git --no-pager branch"
 alias gbc="git branch --contains"
